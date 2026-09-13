23b4fe26a5f3cf70a8436bccd032cdd241c56d6a
Cole Talbot <ctalbot2@student.cscc.edu>
2026-09-13 00:31:56 -0400

    added while loop for printing messages

--- my_project.py
+++ my_project.py
@@ -1,2 +1,6 @@
 sandwich_orders = ['tuna', 'ham', 'turkey']
 finished_sandwiches = []
+
+while sandwich_orders:
+    current_sandwich = sandwich_orders.pop()
+    print(f"I made your {current_sandwich} sandwich")
