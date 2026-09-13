db96596170cac4862e2b10405262de993921a1fe
Cole Talbot <ctalbot2@student.cscc.edu>
2026-09-13 00:40:19 -0400

    added final message for finished sandwiches

--- my_project.py
+++ my_project.py
@@ -5,3 +5,5 @@ while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
     print(f"I made your {current_sandwich} sandwich")
     finished_sandwiches.append(current_sandwich)
+
+print(f"Sandwiches finished: {finished_sandwiches}")
