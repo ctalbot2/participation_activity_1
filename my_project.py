d9e6834324bac63a584647183a6dbd83353c69e0
Cole Talbot <ctalbot2@student.cscc.edu>
2026-09-13 00:37:36 -0400

    finished_sandwiches built from current_sandwich

--- my_project.py
+++ my_project.py
@@ -4,3 +4,4 @@ finished_sandwiches = []
 while sandwich_orders:
     current_sandwich = sandwich_orders.pop()
     print(f"I made your {current_sandwich} sandwich")
+    finished_sandwiches.append(current_sandwich)
